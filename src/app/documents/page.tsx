3a7726bfc994c32a032ff0e0deeede56e385e5df
elizabeth-agada <elizadave6@gmail.com>
2025-03-13 15:03:19 +0100

    dashboard home for databank

---
+++ src/app/documents/page.tsx
@@ -0,0 +1,8 @@
+export default function DocumentPage() {
+    return (
+      <div className="bg-[#040E24] text-white min-h-screen sm:min-h-0 p-6">
+        <h1 className="text-2xl font-semibold mb-6">Documents</h1>
+        <p>Over to you ELIJAH. I am sick of that animation please</p>
+      </div>
+    );
+  }
